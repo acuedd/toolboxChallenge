bcd2c9807e9ee59038eb32c5ca89b64b65d366a4
acuedd <acuedd@gmail.com>
2023-05-13 21:19:48 -0600

    feat: adding loging message to improve ux

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,31 @@ import Layout from './Layout/Layout';
 
 function App() {
   const [reviews, setReviews] = useState([])  
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     fetch('http://localhost:4000/')
       .then(res => res.json())
-      .then(data => setReviews(data))
+      .then((data)=>{
+        setReviews(data)
+        setLoading(false)
+      })  
   }, [])
 
+  console.log(loading,"loading")
+  
   return (
     <div className="App">
       <header className="App-header">
-        <h1>React Test App Toolbox</h1>              
-          {reviews && reviews.map(item => (            
+        <h1>React Test App Toolbox</h1>
+          {loading ? <div>Loading..</div> :
+          reviews && reviews.map(item => (            
             <Layout 
               title={item.file}
               header={item.headers}
               lines={item.lines}
             />            
-          ))}
+          ))}          
       </header>
     </div>
   );
